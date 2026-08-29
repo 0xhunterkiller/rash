245004067a324f18160b412b3f6b39b182760f49
Saai Sudarsanan <saaisudarsanan@gmail.com>
2026-08-29 22:26:12 +0530

    drop priviledges

--- shell.c
+++ shell.c
@@ -7,6 +7,7 @@
 #include <stdarg.h>
 #include <time.h>
 #include <stdbool.h>
+#include <sys/types.h>
 
 void write_log(FILE *log, char *msg, ...){
     if (log == NULL) return;
@@ -26,32 +27,51 @@ void write_log(FILE *log, char *msg, ...){
 }
 
 int main(int argc, char *argv[]) {
+
+    // check access and drop if required
+    // this prevents accessing resoruces with  
+    // sudo or sudo su <higher priviledge user>    
+    char *sudo_uid_str = getenv("SUDO_UID");
+    char *sudo_gid_str = getenv("SUDO_GID");
+    if (sudo_gid_str != NULL && setgid((gid_t)atoi(sudo_gid_str)) < 0) {
+        fprintf(stderr, "cannot sudo into higher priviledge, unable to drop priviledges\n");        
+        return 1;
+    }
+    if (sudo_uid_str != NULL && setuid((uid_t)atoi(sudo_uid_str)) < 0) {
+        fprintf(stderr, "cannot sudo into higher priviledge, unable to drop priviledges\n");        
+        return 1;
+    } 
     
     char *home = getenv("HOME");
     if(home == NULL) return 1;
 
     char workdir[512];
     strcpy(workdir, home);
-    strcat(workdir, "/.local/state/rash");
+    strcat(workdir, "/.local/state/rash");   
 
     char configfile[512];
     strcpy(configfile, workdir);
     strcat(configfile, "/rash.conf");
-
+    
     char logfile[512];
     strcpy(logfile, workdir);
     strcat(logfile, "/rash.log");
 
-    char buffer[512];
-
+    char buffer[512];    
+    
     if (argc < 2) {
         fprintf(stderr, "USAGE: rash <command>\n");
         return 1;
-    }
-
-
+    }    
+    
     FILE *f = fopen(configfile, "r");
     FILE *log = fopen(logfile, "a");
+    
+    if (log == NULL) {
+        fprintf(stderr, "config path doesn't exist\n");
+        perror("rash");
+        return 1;
+    }
 
     if (f == NULL) {
         fprintf(stderr, "rash.conf not found, please create it\n");
