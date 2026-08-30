a428eb6f07f721d9f148ecfca82ee5f2b69ddfc6
Saai Sudarsanan <saaisudarsanan@gmail.com>
2026-08-30 20:57:14 +0530

    its all broken here, watch me fix it.

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <stdbool.h>
 #include <sys/types.h>
+#include <tomlc17.h>
 
 void write_log(FILE *log, char *msg, ...){
     if (log == NULL) return;
@@ -26,6 +27,106 @@ void write_log(FILE *log, char *msg, ...){
     fflush(log);
 }
 
+typedef struct Configuration {
+    bool parse_success;
+    char parser_feedback[200];
+
+    int wl_size;
+    char **wl;
+
+    int env_count; // list of env vars specified excluding PATH and NULL
+    char **allowed_env; // list of valid env vars + constructed PATH + NULL
+} conf;
+
+conf parse_config(FILE *fp) {
+    conf rash_config;
+    toml_result_t result = toml_parse_file(fp);
+
+    rash_config.parse_success = true;
+    strcpy(rash_config.parser_feedback, "");
+
+    if (!result.ok) {
+        rash_config.parse_success = result.ok;
+        strcpy(rash_config.parser_feedback, result.errmsg);
+        return rash_config;
+    }
+
+    // Get Whitelist
+    toml_datum_t whitelist = toml_get(result.toptab, "whitelist");
+    if(whitelist.type == TOML_UNKNOWN) {
+        rash_config.wl = NULL;
+        rash_config.wl_size = 0;
+    }
+
+    if(whitelist.type == TOML_ARRAY) {
+        rash_config.wl = malloc(whitelist.u.arr.size * sizeof(char *));
+        rash_config.wl_size = whitelist.u.arr.size;
+        for(int i=0;i<whitelist.u.arr.size;i++) {
+            toml_datum_t command = whitelist.u.arr.elem[i];
+            if (command.type == TOML_STRING) {
+                rash_config.wl[i] = malloc((strlen(command.u.s)+1) * sizeof(char));
+                strcpy(rash_config.wl[i], command.u.s);
+            }
+        }
+    }
+
+    // Set PATH Variable
+    char *pathenv = "PATH=/usr/local/bin:/usr/bin";
+    
+    // Get Allowed Env
+    int envvarlen = 0;
+    toml_datum_t env_specs = toml_get(result.toptab, "env");
+
+    if(env_specs.type == TOML_ARRAY) {
+        rash_config.env_count = 0;
+        char **valid_envvars = malloc((env_specs.u.arr.size+2) * sizeof(char *));
+        for(int i=0;i<env_specs.u.arr.size;i++){
+            toml_datum_t envvarname = env_specs.u.arr.elem[i];
+            if(envvarname.type == TOML_STRING) {
+                char *tmp = getenv(envvarname.u.s);
+                if (tmp == NULL || strcmp(envvarname.u.s, "PATH") == 0) continue;
+                envvarlen = strlen(tmp)+strlen(envvarname.u.s)+2; // accommodate \0 and the =
+                valid_envvars[rash_config.env_count] = malloc(envvarlen * sizeof(char));
+                snprintf(valid_envvars[rash_config.env_count], envvarlen, "%s=%s", envvarname.u.s, tmp);
+                rash_config.env_count += 1;
+            }
+        }
+        valid_envvars[rash_config.env_count] = pathenv;
+        valid_envvars[rash_config.env_count+1] = NULL;
+        rash_config.allowed_env = valid_envvars;
+    } else {
+        rash_config.allowed_env = malloc(sizeof(char *) * 2);
+        rash_config.allowed_env[0] = pathenv;
+        rash_config.allowed_env[1] = NULL;
+        rash_config.env_count = 0;
+    }
+
+    toml_free(result);
+    return rash_config;
+}
+
+void free_config(conf rash_config) {
+    if (rash_config.wl_size > 0) {
+        for(int i=0;i<rash_config.wl_size;i++){
+            free(rash_config.wl[i]);
+        }
+        free(rash_config.wl);
+    }
+
+    if (rash_config.env_count > 0){
+        for(int i = 0; i < rash_config.env_count; i++) {
+            free(rash_config.allowed_env[i]);
+        }
+    }
+    free(rash_config.allowed_env);
+}
+
+void cleanup(FILE *f, FILE *log, conf rash_config){
+    if (f != NULL) fclose(f);
+    if (log != NULL) fclose(log);
+    free_config(rash_config);
+}
+
 int main(int argc, char *argv[]) {
 
     // check access and drop if required
@@ -35,15 +136,15 @@ int main(int argc, char *argv[]) {
     char *sudo_gid_str = getenv("SUDO_GID");
     if (sudo_gid_str != NULL && setgid((gid_t)atoi(sudo_gid_str)) < 0) {
         fprintf(stderr, "cannot sudo into higher priviledge, unable to drop priviledges\n");        
-        return 1;
+        goto exit_failed;
     }
     if (sudo_uid_str != NULL && setuid((uid_t)atoi(sudo_uid_str)) < 0) {
         fprintf(stderr, "cannot sudo into higher priviledge, unable to drop priviledges\n");        
-        return 1;
+        goto exit_failed;
     } 
     
     char *home = getenv("HOME");
-    if(home == NULL) return 1;
+    if(home == NULL) goto exit_failed;
 
     char workdir[512];
     strcpy(workdir, home);
@@ -51,7 +152,7 @@ int main(int argc, char *argv[]) {
 
     char configfile[512];
     strcpy(configfile, workdir);
-    strcat(configfile, "/rash.conf");
+    strcat(configfile, "/rash.toml");
     
     char logfile[512];
     strcpy(logfile, workdir);
@@ -61,7 +162,7 @@ int main(int argc, char *argv[]) {
     
     if (argc < 2) {
         fprintf(stderr, "USAGE: rash <command>\n");
-        return 1;
+        goto exit_failed;
     }    
     
     FILE *f = fopen(configfile, "r");
@@ -70,40 +171,44 @@ int main(int argc, char *argv[]) {
     if (log == NULL) {
         fprintf(stderr, "config path doesn't exist\n");
         perror("rash");
-        return 1;
+        goto exit_failed;
     }
 
     if (f == NULL) {
-        fprintf(stderr, "rash.conf not found, please create it\n");
+        fprintf(stderr, "rash.toml not found, please create it\n");
         perror("rash");
-        fclose(log);
-        return 1;
-    } 
-
-    bool found = false;    
-    while(fgets(buffer, 512, f) != NULL) {
-        buffer[strcspn(buffer, "\n")] = '\0';
-        if (strcmp(buffer, argv[1]) == 0) {
-            found = true;
-            break;        
-        }
+        goto exit_failed;
     }
-    if(!found){
-        fprintf(stderr, "This command is not allowed: %s\n", argv[1]);
-        write_log(log, "tried to run an unknown command: %s", argv[1]);
-        fclose(f);
-        fclose(log);
-        return 1;
+
+    conf rash_config = parse_config(f);
+    
+    if (!rash_config.parse_success) {
+        fprintf(stderr, "config failed to load: %s\n", rash_config.parser_feedback);
+        return EXIT_FAILURE;
     }
     
-    fclose(f);
+    bool found = false;
+    if (rash_config.wl_size > 0) {
+        for(int i=0;i<rash_config.wl_size;i++){
+            if (strcmp(rash_config.wl[i], argv[1]) == 0) {
+                found = true;
+                break;
+            }
+        }
+    }
+        
+    if(!found){
+        fprintf(stderr, "this command is not allowed: %s\n", argv[1]);
+        write_log(log, "tried to run an unknown command: %s", argv[1]);
+        goto exit_failed;
+    }
 
     int exitcode = 0;
 
     pid_t pid = fork();
 
     if (pid == 0) {
-        execvp(argv[1], &argv[1]);
+        execve(argv[1], &argv[1], rash_config.allowed_env);
         perror("rash");
         exit(errno == ENOENT ? 127 : 126);
     } else if (pid > 0) {
@@ -111,11 +216,20 @@ int main(int argc, char *argv[]) {
         wait(&status);    
         exitcode = WEXITSTATUS(status);
         write_log(log, "ran %s with exit status %d", argv[1], exitcode);
-        fclose(log);
+        cleanup(f, log, rash_config);
         exit(exitcode);
     } else {
-        fclose(log);
+        free_config(rash_config);
+        cleanup(f, log, rash_config);
         exit(1);
     }
+    
+    
+
+    exit_failed:
+        cleanup(f, log, rash_config);    
+        return EXIT_FAILURE;
+
+    
     return 0;
 }
